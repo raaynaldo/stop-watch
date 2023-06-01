b3f2585c1222f2b322db54358fc132cbccaf21c2
Raynaldo Sutisna <raaynaldo@gmail.com>
2023-06-01 00:41:03 -0400

    feat: stopwatch mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+import styles from './App.module.css';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [isRun, setIsRun] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+    if (isRun) intervalId = setInterval(() => setTime((prev) => prev + 1), 10);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [time, isRun]);
+
+  const hour = Math.floor(time / 360000);
+  const minute = Math.floor((time % 360000) / 6000);
+  const second = Math.floor((time % 6000) / 100);
+  const millisecond = time % 100;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.wrapper}>
+      <div className={styles.stopWatch}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span className={styles.time}>
+            {hour.toString().padStart(2, '0')}
+          </span>
+          <span>:</span>
+          <span className={styles.time}>
+            {minute.toString().padStart(2, '0')}
+          </span>
+          <span>:</span>
+          <span className={styles.time}>
+            {second.toString().padStart(2, '0')}
+          </span>
+          <span>.</span>
+          <span className={styles.time}>
+            {millisecond.toString().padStart(2, '0')}
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => setIsRun((prev) => !prev)}>
+          {isRun ? 'Pause' : 'Start'}
+        </button>{' '}
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
     </div>
   );
 }
